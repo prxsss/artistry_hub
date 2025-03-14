d70b2d47a1217f0e52734a31a8bb0b80183ffa17
prxsss <9phuriphat.k@gmail.com>
2025-03-14 14:07:43 +0700

    Add slidable list items to ArtistryHub with edit and delete actions

--- lib/pages/artistry_hub.dart
+++ lib/pages/artistry_hub.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ArtistryHub extends StatefulWidget {
@@ -13,7 +14,55 @@ class _ArtistryHubState extends State<ArtistryHub> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Artistry Hub")),
-      body: const Center(child: Text("Artistry Hub")),
+      body: Padding(
+        padding: const EdgeInsets.all(14),
+        child: ListView.separated(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return Slidable(
+              endActionPane: ActionPane(
+                motion: StretchMotion(),
+                children: [
+                  SlidableAction(
+                    onPressed: (context) {},
+                    backgroundColor: Theme.of(context).colorScheme.primary,
+                    icon: FontAwesomeIcons.pen,
+                    label: "Edit",
+                  ),
+                  SlidableAction(
+                    onPressed: (context) {},
+                    backgroundColor: Theme.of(context).colorScheme.error,
+                    icon: FontAwesomeIcons.trash,
+                    label: "Delete",
+                  ),
+                ],
+              ),
+              child: ListTile(
+                leading: Container(
+                  width: 50,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage(
+                        'https://avatar.iran.liara.run/public/boy?username=Leonardo_da_Vinci',
+                      ),
+                    ),
+                  ),
+                ),
+                title: const Text("Leonardo da Vinci"),
+                subtitle: Row(
+                  children: const [
+                    FaIcon(FontAwesomeIcons.solidFlag, size: 15),
+                    SizedBox(width: 10),
+                    Text("Italian"),
+                  ],
+                ),
+              ),
+            );
+          },
+          separatorBuilder: (context, index) => const Divider(),
+        ),
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: FaIcon(FontAwesomeIcons.plus),
