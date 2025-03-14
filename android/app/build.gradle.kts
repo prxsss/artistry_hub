e8b855fa62b28248783461d876bd23eac653dfd9
prxsss <9phuriphat.k@gmail.com>
2025-03-14 10:59:51 +0700

    Update NDK version and initialize Firebase in main function

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 android {
     namespace = "com.example.artistry_hub"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
